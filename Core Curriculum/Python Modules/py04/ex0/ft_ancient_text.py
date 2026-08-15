6273d4d9bc1eb220c5764741db5ad33f7781d9b1
cabboud28 <christel.abboud321@gmail.com>
2026-08-15 13:25:08 +0300

    added some code of module py04

---
+++ Core Curriculum/Python Modules/py04/ex0/ft_ancient_text.py
@@ -0,0 +1,18 @@
+import sys
+import typing
+
+
+def file_handler():
+    if sys.argv != 2:
+        print("Usage: ft_ancient_text.py <file>")
+        return
+    else:
+        
+        
+
+
+def main() -> None:
+    print("=== Cyber Archives Recovery ===")
+
+
+#file name comes from command line mch static
