d81bd82e2f97140d6059f4d3e4f4c33a0c6e5dec
christel abboud <christel.abboud321@gmail.com>
2026-08-15 21:17:58 +0300

    Refactor file_handler to main and improve argument check

--- Core Curriculum/Python Modules/py04/ex0/ft_ancient_text.py
+++ Core Curriculum/Python Modules/py04/ex0/ft_ancient_text.py
@@ -2,17 +2,30 @@ import sys
 import typing
 
 
-def file_handler():
-    if sys.argv != 2:
+def main() -> None:
+    if len(sys.argv) != 2:
         print("Usage: ft_ancient_text.py <file>")
         return
-    else:
-        
-        
 
-
-def main() -> None:
     print("=== Cyber Archives Recovery ===")
+    print(f"Accessing file: '{sys.argv[1]}'")
+    try:
+        filename: typing.IO = open(sys.argv[1], "r")
+    except Exception as e:
+        print(f"Error opening file '{sys.argv[1]}': {e}")
+        return
+    
+    try:
+        content = filename.read()
+        print("---\n")
+        print(content)
+        print("\n---")
+    finally:
+        filename.close()
+        print(f"File '{sys.argv[1]}' closed")
 
 
-#file name comes from command line mch static
+if __name__ == "__main__":
+    main()
+
+#file name comes from command line (dynamic) not hardcoded --> sys.argv
