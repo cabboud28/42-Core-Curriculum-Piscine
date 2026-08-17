f8ee3356ba2712f906b950ce64f5492e61c1f326
cabboud28 <christel.abboud321@gmail.com>
2026-08-17 11:11:30 +0300

    added the rest of the exs

--- Core Curriculum/Python Modules/py04/ex1/ft_archive_creation.py
+++ Core Curriculum/Python Modules/py04/ex1/ft_archive_creation.py
@@ -23,7 +23,37 @@ def main() -> None:
     finally:
         filename.close()
         print(f"File '{sys.argv[1]}' closed")
-#to be continued
+
+    print("Transform data:")
+    lines = content.splitlines()
+    new_content = ""
+    for line in lines:
+        new_content += line + "#\n"
+
+    print("---\n")
+    print(new_content)
+    print("---")
+
+    new_filename = input("Enter new filename (or empty): ")
+
+    if new_filename == "":
+        print("Not saving data.")
+        return
+
+    print(f"Saving data to '{new_filename}'")
+
+    try:
+        out_file: typing.IO = open(new_filename, "w")
+    except Exception as e:
+        print(f"Error opening file '{new_filename}': {e}")
+        return
+
+    try:
+        out_file.write(new_content)
+    finally:
+        out_file.close()
+
+    print(f"Data saved in file '{new_filename}'")
 
 
 if __name__ == "__main__":
