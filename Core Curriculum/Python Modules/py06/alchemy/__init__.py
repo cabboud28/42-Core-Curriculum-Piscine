35e40f5f6b538940804c2ac740e42ba796192ab3
christel abboud <christel.abboud321@gmail.com>
2026-09-05 04:04:50 +0300

    Add files via upload

---
+++ Core Curriculum/Python Modules/py06/alchemy/__init__.py
@@ -0,0 +1,10 @@
+from .elements import create_air
+from .potions import healing_potion as heal
+from .potions import strength_potion
+
+
+__all__ = [
+    "create_air"
+    "heal"
+    "strength_potion"
+]
